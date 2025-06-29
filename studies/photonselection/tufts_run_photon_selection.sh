93428a5b1b81189ae9a6a10b946b6d8016baae2a
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2025-06-29 17:58:26 -0400

    add scripts to run on tufts

---
+++ studies/photonselection/tufts_run_photon_selection.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+JOBSTARTDATE=$(date)
+
+RECO_FILELIST=$1
+DLMERGED_FILELIST=$2
+SAMPLENAME=$3
+NFILES=$4
+MCFLAG=$5
+ubdlDir=/cluster/tufts/wongjiradlabnu/twongj01/gen2/photon_analysis/ubdl/
+outDir=/cluster/tufts/wongjiradlabnu/twongj01/gen2/gen2ntuple/studies/photonselection/output/${SAMPLENAME}/
+PHOTON_SELECTION_DIR=/cluster/tufts/wongjiradlabnu/twongj01/gen2/gen2ntuple/studies/photonselection/
+
+localDir=`printf /tmp/calc_photonselection_jobarrayid%05d ${SLURM_ARRAY_TASK_ID}`
+mkdir -p ${localDir}
+mkdir -p ${outDir}
+
+source ${ubdlDir}/setenv_py3_container.sh
+source ${ubdlDir}/configure_container.sh
+export PYTHONPATH=${PYTHONPATH}:${scriptDir}
+export PATH=${PHOTON_SELECTION_DIR}/build/installed/bin:${PATH}
+
+cd ${localDir}
+
+maxFileCount=`wc -l < $kpsRecoFiles`
+let firstfile="${SLURM_ARRAY_TASK_ID}*${NFILES}+1"
+let lastfile="${firstfile}+$NFILES-1"
+echo "filest to run between first=${firstfile} to last=${lastfile}"
+for n in $(seq $firstfile $lastfile); do
+  if (($n > $maxFileCount)); then
+    break
+  fi
+
+  fileidx=`sed -n ${n}p ${RECO_FILELIST} | awk '{ print $1 }'`
+  recofile=`sed -n ${n}p ${RECO_FILELIST} | awk '{ print $2 }'`
+  let mergedfile_lineno="${fileidx}+1"
+  mergedfile=`sed -n ${mergedfile_lineno}p ${DLMERGED_FILELIST}`
+  recobase=`basename ${recofile}`
+  outfile=`echo ${recobase} | sed 's|larflowreco|photonselection|g'`
+
+  fileOutDir=`printf ${outDir}/ntuplefile%05d ${SLURM_ARRAY_TASK_ID}`
+  mkdir -p ${fileOutDir}
+  
+  echo "mergedfile: ${mergedfile}"
+  echo "recobase: ${recobase}"
+  echo "outfile: ${outfile}"
+  CMD="make_photon_selection_study_tree ${mergedfile} ${recofile} ${outfile} --tickbackward"
+  echo ${CMD}
+  ${CMD}
+  
+  echo "copy outfile to ${fileOutDir}"
+  cp ${outfile} ${fileOutDir}/${outfile}
+done
+
+# clean-up
+cd /tmp
+rm -r ${localDir}
+
+JOBENDDATE=$(date)
+
+echo "Job began at $JOBSTARTDATE"
+echo "Job ended at $JOBENDDATE"
+
