c74833feb6e5204682d0fc19cb6b15f2f16172e9
twongjirad <taritree.wongjirad@gmail.com>
2025-10-29 20:54:11 -0400

    save new study program folder to research photon interaction building

---
+++ studies/photonbuilding/src/photonbuilding.cxx
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main( int nargs, char** argv ) {
+
+  std::cout << "Building photon-interactions" << std::endl;
+
+  // what is holding us back?
+  // what from the lantern outputs do we have to help us?
+
+  // are we saving information or are we gathering summery statistics?
+
+  // 
+
+
+  return 0;
+  
+}
